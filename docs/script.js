642441ec88b7a0ea801281c91a5ce6fce544c5e2
Takashi Kanemoto <ttskch@gmail.com>
2019-04-25 10:02:36 +0900

    docs: fix width parameter of select2 (see #9 and https://select2.org/appearance\#container-width)

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ $(function () {
   $('select').each(function () {
     $(this).select2({
       theme: 'bootstrap4',
-      width: '100%',
+      width: 'style',
       placeholder: $(this).attr('placeholder'),
       allowClear: Boolean($(this).data('allow-clear')),
     });
