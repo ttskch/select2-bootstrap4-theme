f0098aae96c818cca23cfdfc8c838c6bd299dfc4
Takashi Kanemoto <ttskch@gmail.com>
2020-04-24 17:48:09 +0900

    docs: ✏️ tweak demo

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,8 @@ $(function () {
   $('select').each(function () {
     $(this).select2({
       theme: 'bootstrap4',
-      width: 'style',
-      placeholder: $(this).attr('placeholder'),
+      width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+      placeholder: $(this).data('placeholder'),
       allowClear: Boolean($(this).data('allow-clear')),
     });
   });
