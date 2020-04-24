896f395cc59fa31be5c6cb0ca85339710ee3a3ee
Takashi Kanemoto <ttskch@gmail.com>
2020-04-24 17:47:22 +0900

    style: 💄 just cosme indents

--- webpack.config.js
+++ webpack.config.js
@@ -6,63 +6,63 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 
 let plugins = [
-    new ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-        Popper: ['popper.js', 'default'],
-    }),
-    new ExtractTextPlugin({
-        filename: '[name]' + (isProd ? '.min' : '') + '.css',
-        allChunks: true,
-    }),
+  new ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default'],
+  }),
+  new ExtractTextPlugin({
+    filename: '[name]' + (isProd ? '.min' : '') + '.css',
+    allChunks: true,
+  }),
 ];
 
 module.exports = {
-    entry: {
-        'select2-bootstrap4': [
-            './src/layout.scss',
-        ],
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js',
-    },
-    module: {
-        rules: [
+  entry: {
+    'select2-bootstrap4': [
+      './src/layout.scss',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: () => {
-                                    let plugins = [
-                                        require('precss'),  // bootstrap4 requires this
-                                        require('autoprefixer'),
-                                    ];
-                                    if (isProd) {
-                                      plugins = plugins.concat([
-                                        require('cssnano')({ preset: 'default' }),
-                                      ])
-                                    }
-
-                                    return plugins;
-                                },
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                        },
-                    ],
-                }),
+              loader: 'css-loader',
             },
-        ],
-    },
-    plugins: plugins,
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => {
+                  let plugins = [
+                    require('precss'),  // bootstrap4 requires this
+                    require('autoprefixer'),
+                  ];
+                  if (isProd) {
+                    plugins = plugins.concat([
+                      require('cssnano')({ preset: 'default' }),
+                    ])
+                  }
+
+                  return plugins;
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        }),
+      },
+    ],
+  },
+  plugins: plugins,
 };
