47f9d832ef554fb7e9ca918c47d5d4ebff145153
Takashi Kanemoto <ttskch@gmail.com>
2021-01-22 18:49:48 +0900

    docs: ✏️ tweak demo

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,7 @@ $(function () {
       width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
       placeholder: $(this).data('placeholder'),
       allowClear: Boolean($(this).data('allow-clear')),
+      closeOnSelect: !$(this).attr('multiple'),
     });
   });
 });
