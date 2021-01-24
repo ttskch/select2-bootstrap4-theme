5befae32b4d64d9ee4f8ffdd77ee4c694cf0d465
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-01-24 11:13:41 +0100

    Add stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const ProvidePlugin = require('webpack/lib/ProvidePlugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
 
 module.exports = (env, argv) => {
   const isProd = argv.mode === 'production'
@@ -16,6 +17,9 @@ module.exports = (env, argv) => {
     new MiniCssExtractPlugin({
       filename: '[name]' + (isProd ? '.min' : '') + '.css',
     }),
+    new StyleLintPlugin({
+      files: './src/**/*.(s(c|a)ss|css)'
+    })
   ]
 
   return {
